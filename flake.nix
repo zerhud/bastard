ad0278ad196d74a76f85202602205f9af7fbc5c1
zerhud <zerhud@gmail.com>
2023-08-05 17:59:33 +0300

    add flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "cpp jinja interpretator with exnensions";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    ascip.url = "github:zerhud/ascip";
+    ascip.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = params@{ self, nixpkgs, flake-utils, ascip }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      #ascip = import params.ascip { inherit system; };
+      der = pkgs.gcc13Stdenv.mkDerivation {
+        name = "bastard";
+        nativeBuildInputs = [pkgs.clang_15 ascip.packages."${system}".default];
+        installPhase = "mkdir -p \"$out/include\" && cp ascip.hpp -t \"$out/include\"";
+        buildPhase = "g++ -std=c++23 -fwhole-program -march=native ./test.cpp -o ascip_test && ./ascip_test";
+        meta.description = "bastard is an jinja interpretator with extensions writted in cpp. it can to be embbadded in your projects with open source license.";
+        src = ./.;
+      };
+    in rec {
+      devShell = der;
+      packages.default = der;
+      packages.bastard = der;
+      defaultPackage = der;
+    });
+}
