56ecb803ddfafc6cc7b9479b539a647ea382f81c
zerhud <zerhud@gmail.com>
2024-10-10 00:50:41 +0300

    clang can compile arrays.cpp test
    so we can remove CTRT macro and ran all tests in compile time

--- absd/tests/arrays.cpp
+++ absd/tests/arrays.cpp
@@ -9,53 +9,49 @@
 #include "tests/factory.hpp"
 #include "absd.hpp"
 
-#ifndef __clang__
-#define CTRT(code) static_assert( code );
-#else
-#include <cassert>
-#define CTRT(code) assert( code );
-#endif
-
 template<typename factory>
 constexpr void main_test() {
 	using data = absd::data<factory>;
 	using integer_t = data::integer_t;
-	CTRT(data{}.mk_empty_array().is_array() == true )
-	CTRT(data{}.mk_empty_array().is_object() == false )
-	CTRT(data{(integer_t)10}.mk_empty_array().is_array() == true )
-	CTRT([]{ data d; d.mk_empty_array(); d.push_back(data{(integer_t)10}); return (integer_t)d[0]; }() == 10 )
-	CTRT([]{ data d; d.mk_empty_array(); d.push_back(data{(integer_t)10}); auto dd = d; return (integer_t)dd[0]; }() == 10 )
-	CTRT([]{ data d; d.mk_empty_array(); d.push_back(data{(integer_t)10}); auto dd = std::move(d); return (integer_t)dd[0]; }() == 10 )
-	CTRT([]{ data d; d.mk_empty_array(); d.push_back(data{(integer_t)10}); auto dd = std::move(d); return dd.size(); }() == 1 )
 
-	CTRT( []{
+	static_assert( data{}.mk_empty_array().is_array() == true );
+	static_assert( data{}.mk_empty_array().is_object() == false );
+	static_assert( data{(integer_t)10}.mk_empty_array().is_array() == true );
+
+	constexpr auto push_back_10 = []{
+		data d;
+		d.push_back(data{(integer_t)10});
+		return d;
+	};
+	static_assert( (integer_t)(push_back_10()[0]) == 10 );
+	static_assert( (integer_t)(push_back_10()[data{0}]) == 10 );
+	static_assert( [=]{ data d = push_back_10(); return (integer_t)d[0]; }() == 10 );
+	static_assert( [=]{ data d = push_back_10(); auto dd = d; return (integer_t)dd[0]; }() == 10 );
+	static_assert( [=]{ data d = push_back_10(); auto dd = std::move(d); return (integer_t)dd[0]; }() == 10 );
+	static_assert( [=]{ data d = push_back_10(); auto dd = std::move(d); return dd.size(); }() == 1 );
+
+	static_assert( []{
 		auto vec = mk_vec<data>(factory{});
 		vec.emplace_back(data{(integer_t)1});
 		vec.emplace_back(data{(integer_t)3});
 		auto d = data::mk(std::move(vec));
-		return (integer_t)d[1]; }() == 3)
+		return (integer_t)d[1]; }() == 3);
 
-	CTRT( []{
-		data d;
-		d.push_back(data{3});
-		return (integer_t)d[data{0}];
-	}() == 3 )
-
-	CTRT( []{
+	static_assert( []{
 		data d;
 		d.push_back(data{2});
 		d.push_back(data{3});
 		return d.contains(data{3}) + !d.contains(data{1});
 	}() == 2 );
 
-	CTRT( ([]{
+	static_assert( ([]{
 		data l,r;
 		l.push_back(data{2});
 		r.push_back(data{2});
 		return l == r;
 	}() == true) );
 
-	CTRT( ([]{
+	static_assert( ([]{
 		data l,r;
 		l.push_back(data{2});
 		r.mk_empty_object();
