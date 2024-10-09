2af03ee3b60ab3205c62e65fcd6a7ff698e42c06
zerhud <zerhud@gmail.com>
2024-10-10 01:57:54 +0300

    clang can compile objects.cpp with static_asserts

--- absd/tests/objects.cpp
+++ absd/tests/objects.cpp
@@ -10,54 +10,47 @@
 
 #include "absd.hpp"
 
-#ifndef __clang__
-#define CTRT(code) static_assert( code );
-#else
-#include <cassert>
-#define CTRT(code) assert( code );
-#endif
-
 template<typename factory>
 constexpr void main_test() {
 	using absd_data = absd::data<factory>;
 	using integer_t = absd_data::integer_t;
-	CTRT( absd_data{}.mk_empty_object().is_object() == true );
-	CTRT( absd_data{}.mk_empty_object().is_array() == false );
-	CTRT( []{absd_data d{}; d.mk_empty_object(); d.put(absd_data{1}, absd_data{7}); return (integer_t)d[absd_data{1}];}() == 7 )
-	CTRT( []{absd_data d{}; d.put(absd_data{1}, absd_data{7}); return (integer_t)d[absd_data{1}];}() == 7 )
-	CTRT( []{ absd_data d;
+	static_assert( absd_data{}.mk_empty_object().is_object() == true );
+	static_assert( absd_data{}.mk_empty_object().is_array() == false );
+	static_assert( []{absd_data d{}; d.mk_empty_object(); d.put(absd_data{1}, absd_data{7}); return (integer_t)d[absd_data{1}];}() == 7 );
+	static_assert( []{absd_data d{}; d.put(absd_data{1}, absd_data{7}); return (integer_t)d[absd_data{1}];}() == 7 );
+	static_assert( []{ absd_data d;
 		d.put(absd_data{1}, absd_data{7});
 		d.put(absd_data{2}, absd_data{8});
 		auto keys = d.keys();
-		return (keys.size() == 2) + (2*((integer_t)keys[0] == 1)) + (4*((integer_t)keys[1] == 2)); }() == 7 )
-	CTRT( ([]{
+		return (keys.size() == 2) + (2*((integer_t)keys[0] == 1)) + (4*((integer_t)keys[1] == 2)); }() == 7 );
+	static_assert( ([]{
 		absd::details::constexpr_kinda_map<factory,absd_data,absd_data> v;
 		v.insert(std::make_pair(absd_data{1}, absd_data{2}));
 		return v.contains(absd_data{1});
-	}()) )
-	CTRT( ((integer_t)[]{
+	}()) );
+	static_assert( ((integer_t)[]{
 		absd::details::constexpr_kinda_map<factory,absd_data,absd_data> v;
 		v.insert(std::make_pair(absd_data{1}, absd_data{3}));
-		return absd_data::mk(std::move(v))[absd_data{1}];}() == 3) )
+		return absd_data::mk(std::move(v))[absd_data{1}];}() == 3) );
 
-	CTRT( []{
+	static_assert( []{
 		absd_data d;d.mk_empty_object();
 		d.put(absd_data{1}, absd_data{7});
 		return d.contains(absd_data{1}) + (2*!d.contains(absd_data{7}));
-	}() == 3)
+	}() == 3);
 
-	CTRT( ([]{
+	static_assert( ([]{
 		absd_data l,r;
 		l.mk_empty_object();
 		r.mk_empty_object();
 		return l == r;
-	}() == true) )
-	CTRT( ([]{
+	}() == true) );
+	static_assert( ([]{
 		absd_data l,r;
 		l.put(absd_data{1}, absd_data{7});
 		r.put(absd_data{1}, absd_data{7});
 		return l == r;
-	}() == true) )
+	}() == true) );
 }
 
 template<typename fp> struct absd_factory : tests::factory {};
