c4374867868bb9caa918341877fca02d4ab64f60
zerhud <zerhud@gmail.com>
2024-10-10 01:47:43 +0300

    use mostly static_assert in mk.cpp. clang cannot compile only one test also add test for an exception type.

--- absd/tests/mk.cpp
+++ absd/tests/mk.cpp
@@ -18,6 +18,13 @@
 #define CTRT(code) assert( code );
 #endif
 
+struct test_exception {};
+struct ex_factory : tests::factory {
+	template<typename>
+	[[noreturn]] static void throw_wrong_interface_error() {
+		throw test_exception{};
+	}
+};
 using absd_data = absd::data<tests::factory>;
 
 
@@ -26,20 +33,21 @@ static_assert( absd_data::mk(0.1).is_float_point() );
 
 namespace object_tests {
 
-struct constant_object {
-	absd_data m0{3}, m1{4}, empty;
-	constexpr absd_data& at(absd_data key) {
+template<typename data_type> struct constant_object {
+	using integer_t = data_type::integer_t;
+	data_type m0{3}, m1{4}, empty;
+	constexpr data_type& at(data_type key) {
 		if(!key.is_int()) return empty;
-		if((absd_data::integer_t)key == 0) return m0;
-		if((absd_data::integer_t)key == 1) return m1;
+		if((integer_t)key == 0) return m0;
+		if((integer_t)key == 1) return m1;
 		return empty;
 	}
-	constexpr bool contains(const absd_data& key) const {
-		return key.is_int() && (absd_data::integer_t)key == 0 && (absd_data::integer_t)key == 1;
+	constexpr bool contains(const data_type& key) const {
+		return key.is_int() && (integer_t)key == 0 && (integer_t)key == 1;
 	}
 	constexpr std::size_t size() const { return 2; }
-	constexpr std::vector<absd_data> keys() const {
-		return {absd_data{0}, absd_data{1}};
+	constexpr std::vector<data_type> keys() const {
+		return {data_type{0}, data_type{1}};
 	}
 };
 
@@ -81,24 +89,27 @@ struct constant_array_and_obj {
 
 template<bool run_ct>
 constexpr void test() {
-	CTRT( absd_data::mk(1) == absd_data{1} );
-	CTRT( absd_data::mk(constant_object{}).is_object() );
-	CTRT( absd_data::mk(constant_object{}).size() == 2 );
-	CTRT( absd_data::mk(constant_object{})[absd_data{0}] == absd_data{3} );
-	CTRT( absd_data::mk(constant_object{}).keys().size() == 2 );
-	CTRT( absd_data::mk(constant_array{}).is_array() );
-	CTRT( absd_data::mk(constant_array_and_obj{}).is_array() );
-	CTRT( absd_data::mk(constant_array_and_obj{}).is_object() );
+	using obj = constant_object<absd_data>;
+	static_assert( absd_data::mk(1) == absd_data{1} );
+	static_assert( absd_data::mk(obj{}).is_object() );
+	static_assert( absd_data::mk(obj{}).size() == 2 );
+	static_assert( absd_data::mk(obj{})[absd_data{0}] == absd_data{3} );
+	static_assert( absd_data::mk(obj{}).keys().size() == 2 );
+	static_assert( absd_data::mk(constant_array{}).is_array() );
+	static_assert( absd_data::mk(constant_array_and_obj{}).is_array() );
+	static_assert( absd_data::mk(constant_array_and_obj{}).is_object() );
 	CTRT( absd_data::mk(constant_array_and_obj{})[absd_data{"m0"}].is_int() );
-	CTRT( absd_data::mk(constant_array_and_obj{})[absd_data{0}].is_int() );
+	static_assert( absd_data::mk(constant_array_and_obj{})[absd_data{0}].is_int() );
 }
 
-void cannt_modify() {
+void cannot_modify() {
 	int ex = 0;
+	using ex_data = absd::data<ex_factory>;
+	using obj = constant_object<ex_data>;
 	try {
-		absd_data::mk(constant_object{}).put(absd_data{1},absd_data{2});
+		ex_data::mk(obj{}).put(ex_data{1},ex_data{2});
 	}
-	catch(const std::exception&) {
+	catch(const test_exception&) {
 		++ex;
 	}
 	assert( ex == 1 );
@@ -108,6 +119,6 @@ void cannt_modify() {
 
 int main(int,char**) {
 	object_tests::test<false>();
-	object_tests::cannt_modify();
+	object_tests::cannot_modify();
 	return 0;
 }
