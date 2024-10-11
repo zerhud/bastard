dfbc3bc5156d88b80117047c939032877877fea2
zerhud <zerhud@gmail.com>
2024-10-11 16:30:51 +0300

    move callable tests to tests/callable.cpp

--- absd/tests/callable.cpp
+++ absd/tests/callable.cpp
@@ -22,6 +22,25 @@ constexpr void main_test() {
 	using data_type = absd::data<factory>;
 	using int_t = data_type::integer_t;
 
+	static_assert( data_type::mk_ca([]{return 1;})() == 1 );
+	static_assert( data_type::mk_ca([](int i){return i+1;})(3) == 4 );
+	static_assert( data_type::mk_ca([](int a, int b){return a+b;})(3,2) == 5 );
+	CTRT( data_type::mk_ca(
+			[](int a, int b){return a+b;},
+			data_type::mk_param("a"),
+			data_type::mk_param("b", data_type{3})
+	)(1) == 4);
+	static_assert( []{
+		int ret=2;
+		data_type::mk_ca([&ret](int){++ret;})(3);
+		return ret;
+	}() == 3, "void lambda should to be called with () operator");
+	CTRT( []{
+		int ret=2;
+		auto result = data_type::mk_ca([&ret](int){++ret;}, data_type::mk_param("a")).call(data_type::mk_map(0, 3));
+		return (ret==3) + result.is_none();
+	}() == 2 );
+
 	static_assert( !data_type::mk([]{}).is_array() );
 	static_assert( !data_type::mk([]{}).is_object() );
 	static_assert( data_type::mk([]{}).is_callable() );
@@ -32,9 +51,12 @@ constexpr void main_test() {
 	CTRT( (int_t)data_type::mk([](int a, int b){return a-b;}, data_type::mk_param("a"), data_type::mk_param("b", data_type{1})).call(data_type::mk_map(0, 3)) == 2);
 
 	constexpr auto amb = []{return data_type::mk([](int a, int b){return a-b;}, data_type::mk_param("a", data_type{3}), data_type::mk_param("b")); };
-	CTRT( (int_t)amb().call(data_type::mk_map("b", 3)) == 0);
-	CTRT( (int_t)amb().call(data_type::mk_map(0, 7, "b", 3)) == 4);
-	CTRT( (int_t)amb().call(data_type::mk_map("b", 3, 0, 7)) == 4);
+	CTRT( (int_t)amb().call(data_type::mk_map(0, 3, 1, 1)) == 2 );
+	CTRT( (int_t)amb().call(data_type::mk_map("a", 3, "b", 1)) == 2 );
+	CTRT( (int_t)amb().call(data_type::mk_map("b", 1, "a", 3)) == 2 );
+	CTRT( (int_t)amb().call(data_type::mk_map("b", 3)) == 0 );
+	CTRT( (int_t)amb().call(data_type::mk_map(0, 7, "b", 3)) == 4 );
+	CTRT( (int_t)amb().call(data_type::mk_map("b", 3, 0, 7)) == 4 );
 
 	struct callable_obj_arr : absd::details::constexpr_kinda_map<typename data_type::factory_t, data_type, data_type> {
 		mutable data_type fake_data;
@@ -42,7 +64,7 @@ constexpr void main_test() {
 		constexpr data_type operator()() const {return data_type{88};}
 		constexpr data_type operator()(int_t a, int_t b) const {return data_type{a-b};}
 		constexpr data_type& at(int_t ind) const { if(ind==0) return data_3; if(ind==1) return data_5; return fake_data; }
-		constexpr data_type& emplace_back(data_type d) { return fake_data; }
+		constexpr data_type& emplace_back(data_type) { return fake_data; }
 		//constexpr int_t size() const { return 2; }
 		//constexpr bool contains(const data_type&) const { return false; }
 	};
