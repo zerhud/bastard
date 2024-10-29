fab817e405dcb8420208a3a2653a3dc80a62eced
zerhud <zerhud@gmail.com>
2024-10-30 00:25:10 +0300

    add set to common factory. vector will be used as set in constexpr functions.

--- tests/common_factory.hpp
+++ tests/common_factory.hpp
@@ -10,10 +10,17 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_set>
 
 namespace tests {
 
-struct common_factory { };
+struct common_factory {
+	template<typename _t> struct _type_c { using type = _t; };
+	template<typename type> constexpr static auto mk_set_type() {
+		if consteval { return _type_c<std::vector<type>>{}; }
+		else { return _type_c<std::unordered_set<type>>{}; }
+	}
+};
 
 constexpr auto mk_ptr(const common_factory&, auto d) {
 	return std::make_unique<decltype(d)>( std::move(d) );
@@ -27,6 +34,20 @@ template<typename type> constexpr auto mk_vec(const common_factory&, auto&&... i
 	std::vector<type> ret{};
 	if constexpr(sizeof...(items) > 0)
 		(void)(ret.emplace_back(std::forward<decltype(items)>(items)), ...);
+	return ret;
+}
+
+template<typename type> constexpr auto mk_set(const common_factory& f, auto&&... items) {
+	using set_type = decltype(f.mk_set_type<type>())::type;
+	set_type ret{};
+	if constexpr (sizeof...(items) > 0) {
+		auto first = [](auto&& first, auto&&...){return std::forward<decltype(first)>(first);};
+		if constexpr( requires{ret.emplace_back(first(std::forward<decltype(items)>(items)...));} )
+			(void)(ret.emplace_back(std::forward<decltype(items)>(items)),...);
+		else if constexpr ( requires{ret.empalce(first(std::forward<decltype(items)>(items)...));})
+			(void)(ret.emplace(std::forward<decltype(items)>(items)), ...);
+		else static_assert( false, "emplace items: operation not supported");
+	}
 	return ret;
 }
 
