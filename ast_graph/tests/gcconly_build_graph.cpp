3dd5d80751f17589510c32bb40b40389cce8e266
zerhud <zerhud@gmail.com>
2024-10-30 22:15:48 +0300

    add teset_fixture: the IDE cannot parse the mutable keyword in lambda

--- ast_graph/tests/gcconly_build_graph.cpp
+++ ast_graph/tests/gcconly_build_graph.cpp
@@ -62,20 +62,34 @@ struct fixture {
 	}
 };
 
+template<typename test>
+struct test_fixture : fixture, test{
+	using test::operator();
+	constexpr test_fixture(test&& t) : test(std::forward<decltype(t)>(t)) {}
+	constexpr auto operator()() {
+		return (*this)(*this);
+	}
+};
+
 
 static_assert(ast_graph::mk_graph_calc_size(inner_factory{}, test_fields{} ) == 2 );
 static_assert(ast_graph::mk_graph(inner_factory{}, test_fields{} ).size() == 2 );
 static_assert(ast_graph::mk_graph(inner_factory{}, fixture{}.mk_test_fields(2).src ).size() == 6 );
-static_assert( [f=fixture{}]mutable{
+static_assert( test_fixture{[](auto& f){ return f.v.size(); }}() == 2 );
+static_assert( test_fixture{[](auto& f){
+	auto [_,v] = f.mk_test_fields(2).mk_test_graph();
+	return v.size();
+}}() == 2 );
+static_assert( test_fixture{[](auto& f){
 	auto [_,v] = f.mk_test_fields(2).mk_test_graph();
 	return children_of(v, v.root()).size();
-}() == 1 );
+}}() == 1 );
 static_assert(ast_graph::mk_graph(inner_factory{}, fixture{}.mk_test_fields(2).src ).create_view().root()->is_array() == false );
-static_assert( [f=fixture{}]mutable{
+static_assert( test_fixture{[](auto& f){
 	auto [_,v] = f.mk_test_fields(2).mk_test_graph();
 	return children_of(v, v.root())[0]->is_array();
-}() == true );
-static_assert( [f=fixture{}]mutable{
+}}() == true );
+static_assert( test_fixture{[](auto& f){
 	auto [_,v] = f.mk_test_fields(2).mk_test_graph();
 	auto children = v.ast_links_of(v.root());
 	auto& c0 = children[0];
@@ -86,12 +100,12 @@ static_assert( [f=fixture{}]mutable{
 	4*(cn.size() == 2) +
 	8*(cn[0].name == "")
 	;
-}() == 15 );
-static_assert( [f=fixture{}]mutable{
+}}() == 15 );
+static_assert( test_fixture{[](auto& f){
 	f.mk_test_fields(2);
 	f.src.leafs[1].ff = 3;
 	f.src.leafs[0].ff = 3;
-	f.src.leafs[0].vl.emplace<1>().v2f = 7;
+	f.src.leafs[0].vl.template emplace<1>().v2f = 7;
 
 	auto[ g, v ] = f.mk_test_graph();
 	auto children = v.ast_links_of(v.root());
@@ -104,63 +118,58 @@ static_assert( [f=fixture{}]mutable{
 	4*(v.ast_links_of(cn[0].child)[0].name == "vl") +
 	8*(v.ast_links_of(cn[0].child)[0].child->field("v2f") == inner_factory::data_type{7})
 	;
-}() == 15 );
+}}() == 15 );
 
-static_assert( [f=fixture{}]mutable{
+static_assert( test_fixture{[](auto& f){
 	auto [_,v] = f.mk_test_fields(2).mk_test_graph();
 	return v == v;
-}(), "can compare graph view with operator==" );
-static_assert( [f=fixture{}]mutable{
+}}(), "can compare graph view with operator==" );
+static_assert( test_fixture{[](auto& f){
 	auto& src = f.mk_test_fields(2).src;
 	auto g1 = ast_graph::mk_graph(f.f, src);
 	auto g2 = ast_graph::mk_graph(f.f, src);
 	return g1.create_view() == g2.create_view();
-}(), "can compare graph view with operator== on same source");
-static_assert( [f=fixture{}]mutable{
+}}(), "can compare graph view with operator== on same source");
+static_assert( test_fixture{[](auto& f){
 	auto src1 = f.create_test_fields(2);
 	auto src2 = f.create_test_fields(2);
 	auto g1 = ast_graph::mk_graph(f.f, src1);
 	auto g2 = ast_graph::mk_graph(f.f, src2);
 	return g1.create_view() != g2.create_view();
-}(), "can compare graph view with operator!=" );
-static_assert( []{
-	fixture f;
+}}(), "can compare graph view with operator!=" );
+static_assert( test_fixture{[](auto& f){
 	auto [g,_] = f.mk_test_fields(2).mk_test_graph();
 	return g.links_of(g.links_of(g.root().base)[0].child).size();
-}() == 2);
-static_assert( []{
-	fixture f;
+}}() == 2);
+static_assert( test_fixture{[](auto& f){
 	auto [g,v] = f.mk_test_fields(2).mk_test_graph();
 	auto vs = g.create_empty_view();
 	vs.add_vertex(g.root().base);
 	auto& leafs = vs.add_vertex(g.links_of(g.root().base)[0].child);
 	vs.add_vertex(g.links_of(&leafs)[0].child);
 	return (vs.size() == 3) + 2*(vs.ast_links_of(&leafs).size() == 1);
-}() == 3, "get vertices only added to view, not all present in graph");
+}}() == 3, "get vertices only added to view, not all present in graph");
 
-static_assert( []{
-	fixture f;
+static_assert( test_fixture{[](auto& f){
 	auto [g,v] = f.mk_test_fields(2).mk_test_graph();
 	auto vt = g.create_empty_view();
 	vt += v;
 	return v.size() == vt.size();
-}() == true );
-static_assert( []{
-	fixture f;
+}}() == true );
+static_assert( test_fixture{[](auto& f){
 	auto [g,v] = f.mk_test_fields(2).mk_test_graph();
 	auto vt = g.create_empty_view();
 	vt += v;
 	return v.size() == vt.size();
-}() == true );
-static_assert( []{
-	fixture f;
+}}() == true );
+static_assert( test_fixture{[](auto& f){
 	auto [g,v] = f.mk_test_fields(2).mk_test_graph();
 	auto vt = g.create_empty_view();
 	auto orig_size = v.size();
 	vt.add_vertex(g.root().base);
 	v -= vt;
 	return v.size() == orig_size-1;
-}() == true );
+}}() == true );
 
 int main(int,char**) {
 	auto src = fixture{}.create_test_fields(2);
