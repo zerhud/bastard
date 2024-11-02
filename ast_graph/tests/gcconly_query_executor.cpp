7c1a28fdb09a8b29acd8baeccda45b0ed4f66c59
zerhud <zerhud@gmail.com>
2024-11-03 02:08:51 +0300

    remove jiexpr, add fake jiexpr for boost compilation time

--- ast_graph/tests/gcconly_query_executor.cpp
+++ ast_graph/tests/gcconly_query_executor.cpp
@@ -3,7 +3,6 @@
 
 #include "tests/factory.hpp"
 #include "absd/iostream_op.hpp"
-#include "jiexpr.hpp"
 #include "ast_graph/query_executor.hpp"
 #include "ast_graph/absd_object.hpp"
 #include "ascip.hpp"
@@ -45,22 +44,36 @@ struct factory : tests::factory {
 	using parser = ascip<std::tuple>;
 };
 
+struct fake_emb_expr_variant : std::variant<bool, int>{};
+template<typename type>
+constexpr auto& create(fake_emb_expr_variant& v){
+	return v.emplace<type>();
+}
+
+struct fake_emb_expr {
+	using parsed_expression = fake_emb_expr_variant;
+
+	parsed_expression data;
+
+	template<typename gh>
+	friend constexpr auto create_parser(const fake_emb_expr&) {
+		constexpr auto bp = (as<true>(gh::template lit<"true">) | as<false>(gh::template lit<"false">));
+		return use_variant_result(bp) | gh::int_;
+	}
+};
+
 using data_type = factory::data_type;
 using graph_type = decltype(ast_graph::mk_graph(factory{}, test_data::test_fields{}));
-using jiexpr_test = jiexpr<factory>;
 
 struct parser_factory : factory {
 	data_type env;
-	jiexpr_test jiexpr;
+	fake_emb_expr jiexpr;
 
 	constexpr friend const auto& vertex_expression(const parser_factory& f) { return f.jiexpr; }
 	constexpr friend auto solve_vertex(parser_factory& self, const auto& f, const auto& expr, const auto* graph) {
-		data_type env;
-		env.mk_empty_object();
-		env.put(data_type{"v"}, data_type::mk(ast_graph::absd_object(f, graph)));
-		jiexpr_test::solve_info info{};
-		info.env = &env;
-		return expr.solve(info);
+		return visit([](const auto& v){
+			return data_type{(bool)v};
+		}, expr);
 	}
 };
 
@@ -151,46 +164,17 @@ static_assert( test_fixture{[](auto& f){
 	2*(vertex_evaluator{f.f, &f.pf, f.first_leaf()}(qv)==true)
 	;
 }}() == 3, "can find field by field value");
-
-struct fixture {
-	factory f;
-	test_data::test_fields src_st;
-	graph_type graph = ast_graph::mk_graph(f, src_st);
-	jiexpr_test jiexpr;
-	parser_factory pf;
-
-	constexpr fixture() {
-		//pf.jiexpr = &jiexpr;
-	}
-
-	constexpr auto mk_executor() {
-		return ast_graph::query_executor( f, &pf, src_st );
-	}
-
-	constexpr auto mk_obj(auto view, const auto* g) const {
-		return data_type::mk(ast_graph::graph_absd(f, view, g));
-	}
-	constexpr auto mk_obj() const {
-		return mk_obj(graph.create_view(), graph.root().base);
-	}
-};
-
-/*
-static_assert( fixture{}.mk_executor()("{{v.field_1==1}}").size() == 1 );
-static_assert( [f=fixture{}]mutable{
-	auto q = f.mk_executor();
-	auto r = q("{{}}");
-	return (r.size() > 0) + 2*(f.mk_obj() == f.mk_obj(r, r.root()));
-}() == 3 );
-*/
-/*
-static_assert( [f=fixture{}]mutable{
-	auto q = f.mk_executor();
-	f.src_st.leafs.emplace_back().vl.emplace<test_data::variant_leaf_with_name>().name = "test";
-	auto r = q("{'test'}");
-	return r.size();
-}() == 1 );
-*/
+static_assert( test_fixture{[](auto& f){
+	f.create_named("test");
+	auto parsed_true = ast_graph::parse_from(f.pf, "{{true}}");
+	auto parsed_false = ast_graph::parse_from(f.pf, "{{false}}");
+	auto& qt = get<2>(parsed_true.data);
+	auto& qf = get<2>(parsed_false.data);
+	return
+	  (vertex_evaluator{f.f, &f.pf, f.first_named()}(qf)==false) +
+	2*(vertex_evaluator{f.f, &f.pf, f.first_named()}(qt)==true)
+	;
+}}() == 3 );
 
 int main(int,char**) {
 	return 0;
