5b4b088a8c623cc7fff4b6fe12056deff3898f6f
zerhud <zerhud@gmail.com>
2024-12-19 01:25:50 +0300

    add lables to parser tests

--- jinja/tests/parser.cpp
+++ jinja/tests/parser.cpp
@@ -7,7 +7,14 @@
  *************************************************************************/
 
 #include "tests/factory.hpp"
-#include "ascip.hpp"
+#include <ascip.hpp>
+/*
+#ifdef __clang__
+#include "ascip_all_clang.hpp"
+#else
+#include "ascip_all.hpp"
+#endif
+*/
 
 #include "jinja.hpp"
 
@@ -49,7 +56,7 @@ static_assert( []{
 	jinja_details::content<factory> r;
 	auto p = parse(r.mk_parser(), parser::make_source("\\%<<#"), r);
 	return (p == 3) + 2*(r.value == "\\%<");
-}() == 3 );
+}() == 3, "content parser test" );
 static_assert( []{
 	jinja_details::trim_info<factory> r1, r2, r3;
 	const auto p = parse(r1.mk_parser(), parser::make_source("+3+"), r1);
@@ -60,7 +67,7 @@ static_assert( []{
 	8*(p2==1) + 16*(r2.shift==0) + 32*(r2.trim) +
 	64*(p3==0) + 128*(r3.shift==0) + 256*(!r3.trim)
 	;
-}() == 511);
+}() == 511, "trim info parser teset");
 static_assert( []{
 	jinja_details::comment_operator<factory> r1, r2;
 	const auto p1 = parse(r1.mk_parser(), parser::make_source("<# <% <= foo +#>"), r1);
@@ -69,12 +76,12 @@ static_assert( []{
 	  (p1==16) + 2*(r1.value == " <% <= foo ") + 4*r1.end.trim +
 	8*(p2==16) + 16*r2.begin.trim
 	;
-}() == 31 );
+}() == 31, "comment operator parser teset" );
 static_assert( []{
 	jinja_details::expression_operator<factory> r1;
 	const auto p1 = parse(r1.mk_parser(), +parser::space, parser::make_source("<= 3 =>"), r1);
 	return (p1 == 7) + 2*(get<1>(r1.expr)==3);
-}() == 3 );
+}() == 3, "expression operator parser test" );
 
 
 static_assert( []{
@@ -128,7 +135,7 @@ static_assert( [] {
 	+ 2*(static_cast<const jinja_set_b&>(*r1[0]).name() == "name")
 	+ 4*(get<0>(static_cast<const jinja_set_b&>(*r1[0]).handler) == "name")
 	;
-}() == 7 );
+}() == 7, "template with set block" );
 
 int main(int,char**) {
 	return 0;
